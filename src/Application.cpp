9c7dfa29acbb8e3102347407537e6f72878fd591
SamMcguire <sam.mcguire78@gmail.com>
2022-11-02 23:16:11 +1100

    Got GLEW setup

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main()
@@ -14,17 +15,26 @@ int main()
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        return -1;  
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        return -1;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2d(-0.5f, -0.5f);
+        glVertex2d(-0.0f, 0.5f);
+        glVertex2d(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
